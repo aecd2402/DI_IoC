b9469f46747139ca6ecaaafa6898dbf927828199
aecd2402 <aecd2402@gmail.com>
2025-12-03 21:34:02 -0600

    Creacion del proyecto Spring-Boot Patrones de disenio DI y IoC

---
+++ src/test/java/com/spring/patrones/di_ioc/DiIoCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.patrones.di_ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiIoCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
